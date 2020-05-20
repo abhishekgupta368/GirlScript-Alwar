a577519ed08bb456718f8c980d62f4f8eff864ec
abhishekgupta368 <abhigupta9087@gmail.com>
2020-05-20 11:41:41 +0530

    frontTag Added

--- home/models.py
+++ home/models.py
@@ -2,38 +2,38 @@ from django.db import models
 
 # Create your models here.
 class TelegramGroup(models.Model):
-    group_name = models.TextField(max_length=200,default='NULL')
-    contact = models.TextField(max_length=200)
-    telegram_url = models.TextField(max_length=200)
+    group_name = models.TextField(max_length=50,default='NULL')
+    contact = models.TextField(max_length=20)
+    telegram_url = models.TextField(max_length=100)
 
     def __str__(self):
         return self.contact
     
 class WhattsappGroup(models.Model):
-    group_name = models.TextField(max_length=200,default='NULL')
-    contact = models.TextField(max_length=200)
-    whattsapp_url = models.TextField(max_length=200)
+    group_name = models.TextField(max_length=50,default='NULL')
+    contact = models.TextField(max_length=20)
+    whattsapp_url = models.TextField(max_length=100)
 
     def __str__(self):
         return self.contact
     
 class UserFeedback(models.Model):
-    name = models.TextField(max_length=200)
-    email = models.TextField(max_length=200)
-    contact = models.TextField(max_length=200)
+    name = models.TextField(max_length=50)
+    email = models.TextField(max_length=100)
+    contact = models.TextField(max_length=20)
     query = models.TextField()
 
     def __str__(self):
         return self.name
     
 class UserEvent(models.Model):
-    event_name = models.TextField(max_length=200)
+    event_name = models.TextField(max_length=100)
     cover_image = models.ImageField(upload_to='event_image',default='event_image/default.jpg')
-    address = models.TextField(max_length=200)
-    latitude = models.TextField(max_length=200)
-    longitude = models.TextField(max_length=200)
+    address = models.TextField(max_length=100)
+    latitude = models.TextField(max_length=50)
+    longitude = models.TextField(max_length=50)
     event_date = models.DateTimeField(auto_now=False)
-    about_event = models.TextField(max_length=200)
+    about_event = models.TextField()
 
     def __str__(self):
         return self.event_name
@@ -43,12 +43,12 @@ class UserEventImage(models.Model):
     album = models.ImageField(upload_to='album_image',default='album_image/default.jpg')
 
     def __str__(self):
-        return self.event_name
+        return self.event_name.event_name
     
 
 class Schedule(models.Model):
     event_name = models.ForeignKey(UserEvent,on_delete=models.CASCADE)
-    schedule_name = models.TextField(max_length=200)
+    schedule_name = models.TextField(max_length=100)
     start_time = models.TimeField(auto_now=False)
     end_time = models.TimeField(auto_now=False)
     description = models.TextField()
@@ -58,31 +58,31 @@ class Schedule(models.Model):
     
 class Speaker(models.Model):
     event_name = models.ForeignKey(UserEvent,on_delete=models.CASCADE)
-    speaker_name = models.TextField(max_length=200)
+    speaker_name = models.TextField(max_length=50)
     speaker_image = models.ImageField(upload_to='speaker_image',default='speaker_image/default.jpg')
-    github_account = models.TextField(max_length=200)
-    linkedin_account = models.TextField(max_length=200)
-    about_speaker = models.TextField(max_length=200)
+    github_account = models.TextField(max_length=100)
+    linkedin_account = models.TextField(max_length=100)
+    about_speaker = models.TextField(max_length=100)
 
     def __str__(self):
         return self.speaker_name
 
 
 class Team(models.Model):
-    name = models.TextField(max_length=200)
+    name = models.TextField(max_length=100)
     user_image = models.ImageField(upload_to='profile_image',default='profile_image/default.jpg')
-    linkedin_url = models.TextField(max_length=200)
-    github_url = models.TextField(max_length=200)
+    linkedin_url = models.TextField(max_length=100)
+    github_url = models.TextField(max_length=100)
     about_member = models.TextField()
     current_status = models.BooleanField()
-    college = models.TextField(max_length=200)
+    college = models.TextField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class TeamPosition(models.Model):
     name = models.ForeignKey(Team,on_delete=models.CASCADE)
-    position = models.TextField(max_length=200)
+    position = models.TextField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.name.name+" -- " + self.position
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import TelegramGroup
+from .models import WhattsappGroup
+from .models import UserFeedback
+from .models import UserEvent
+from .models import UserEventImage
+from .models import Schedule
+from .models import Speaker
+from .models import Team
+from .models import TeamPosition
 
 # Create your tests here.
+
+# testing database.
+class TelegramGroupTest(TestCase):
+    def test_insert_data(self):
+        TelegramGroup.objects.create(
+                                group_name = "Alwar",
+                                contact = "9087000556",
+                                telegram_url="www.t.me"
+                            )
+        self.assertEqual(TelegramGroup.objects.count(), 1)
+
+class WhattsappGroupTest(TestCase):
+    def test_insert_data(self):
+        WhattsappGroup.objects.create(
+                                group_name = "Alwar",
+                                contact = "9087000556",
+                                whattsapp_url="www.t.me"
+                            )
+        self.assertEqual(WhattsappGroup.objects.count(), 1)
+
+
